86d3579ad46ab95b52dc517909c1544dbcdd495a
Abdelrahman <boda8099@gmail.com>
2024-09-07 20:59:56 +0300

    auth_type: auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth_type = os.getenv('AUTH_TYPE', 'basic')
 auth = None
-if auth:
+if auth_type:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
