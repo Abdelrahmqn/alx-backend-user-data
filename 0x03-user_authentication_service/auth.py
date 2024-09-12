d9870c34ed4c68fb7ac95f2843906681d75f463c
Abdelrahman <boda8099@gmail.com>
2024-09-12 23:24:18 +0300

    add auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""auth
+"""
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> hashpw:
+    """function _hash_password
+        returns: bytes
+        password.encode('utf-8')
+        converts the string password to bytes
+        as hashpw requires byte input tho.
+        """
+    salt = gensalt()
+    return hashpw(password.encode('utf-8'), salt)
