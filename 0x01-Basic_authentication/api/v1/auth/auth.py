4d92c9afbe83408f1c51ecfed2012aa071c01b96
Abdelrahman <boda8099@gmail.com>
2024-09-28 20:04:42 +0300

    b64

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,7 @@ class Auth:
             return None
         return request.headers.get('Authorization', None)
 
-    def current_user(self, request=None) -> TypeVar("User"): # type: ignore
+    def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
         """
         None - request
         """
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,4 +13,34 @@ class BasicAuth(Auth):
     """
     Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(
+                                            self, authorization_header:
+                                            str) -> str:
+        """extract base 64 auth head"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        token = authorization_header.split(" ")[-1]
+        return token
+
+    def decode_base64_authorization_header(
+                                            self,
+                                            base64_authorization_header:
+                                            str) -> str:
+        """decode in base 64 auth header."""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64.b64decode(base64_authorization_header, validate=True)
+            return base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                ).decode('utf-8')
+        except (binascii.Error, ValueError):
+            return None
